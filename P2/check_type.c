7d4bf43f27753a2311178e1fbebc88610ae84239
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2021-05-16 18:52:02 +0530

    wrote a seperate program for checking size type in case I forget.

---
+++ P2/check_type.c
@@ -0,0 +1,14 @@
+/*
+Praneet Kapoor
+16.04.2021
+
+A simple program to check the size of various data-types on my machine.
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+    printf("Size of int is %lu\n", sizeof(int));
+    return 0;
+}
